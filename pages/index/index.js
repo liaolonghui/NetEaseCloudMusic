9074e7b097f7b9e890606df6e0114ae9d1666007
liaolonghui <1340881930@qq.com>
2021-04-15 16:25:57 +0800

    使用后端动态数据

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,14 +1,24 @@
 // index.js
+import request from '../../utils/request'
 // 获取应用实例
 const app = getApp()
 
 Page({
   data: {
-
+    bannerList: [],
+    recommendList: []
   },
 
-  onLoad() {
-
+  onLoad: async function (options) {
+    const bannerData = await request('/banner', {type: 2})
+    this.setData({
+      bannerList: bannerData.banners
+    })
+    // 获取推荐歌单数据
+    const recommendData = await request('/personalized', {limit: 10})
+    this.setData({
+      recommendList: recommendData.result
+    })
   },
 
 })
