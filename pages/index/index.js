e4f4c4db92e2c26cc592ff489d5d894eb67e5d52
liaolonghui <1340881930@qq.com>
2021-04-15 20:26:12 +0800

    完成首页以及主页

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,10 +6,12 @@ const app = getApp()
 Page({
   data: {
     bannerList: [],
-    recommendList: []
+    recommendList: [],
+    topList: []
   },
 
   onLoad: async function (options) {
+    // banner
     const bannerData = await request('/banner', {type: 2})
     this.setData({
       bannerList: bannerData.banners
@@ -19,6 +21,18 @@ Page({
     this.setData({
       recommendList: recommendData.result
     })
+    // 排行榜数据
+    let topList = []
+    let index = 0
+    while (index < 5) {
+      let itemData = await request('/top/list', {idx: index++})
+      let itemList = { name: itemData.playlist.name, tracks: itemData.playlist.tracks.slice(0, 3) }
+      topList.push(itemList)
+      // 为了用户体验应该获取到一次数据就push一次
+      this.setData({
+        topList
+      })
+    }
   },
 
 })
